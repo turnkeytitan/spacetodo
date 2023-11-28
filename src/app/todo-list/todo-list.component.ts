7d6c418b13b66a95be945c222246d7c6d7c53cad
Samuel Sanchez <sm.sanchez.co@gmail.com>
2023-11-28 01:06:37 -0500

    feat: filter completed

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class TodoListComponent {
   newTodoTitle = '';
+  isComplete = false;
   constructor(public todoService: TodoService) {}
 
   addTodo() {
